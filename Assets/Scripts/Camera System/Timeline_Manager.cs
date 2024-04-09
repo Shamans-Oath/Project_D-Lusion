a8da3ed34e153d5c80edf1715de6220e47521a71
Brando Canchanya <brandocandela@gmail.com>
2024-04-09 13:02:20 -0500

    push para Aleksei :v:

---
+++ Assets/Scripts/Camera System/Timeline_Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timeline_Manager : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
