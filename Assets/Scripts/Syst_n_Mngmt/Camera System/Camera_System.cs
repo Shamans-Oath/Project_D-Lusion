77ba964ebab922495869a846f34f35f0d02b5f54
AlekseiPalma <supersonic616@hotmail.com>
2024-05-30 19:16:00 -0500

    Avance en el sistema de efectos de camara

--- Assets/Scripts/Syst_n_Mngmt/Camera System/Camera_System.cs
+++ Assets/Scripts/Syst_n_Mngmt/Camera System/Camera_System.cs
@@ -7,6 +7,9 @@ public class Camera_System : MonoBehaviour
 {
     [SerializeField] private CinemachineFreeLook _playerCamera;
     [SerializeField] private Timeline_Manager _timelineManager;
+
+    public CameraShakeScriptable[] shakeBehaviors;
+    [SerializeField] private CinemachineImpulseSource cmp_CIS;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,24 @@ public class Camera_System : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            CameraShake("TestExplosion");
+        }
     }
 
+    public void CameraShake(string name)
+    {
+        foreach (CameraShakeScriptable camShake in shakeBehaviors)
+        {
+            if (camShake.Name == name)
+            {
+                cmp_CIS.m_ImpulseDefinition = camShake.impulseDefinition;
+                cmp_CIS.m_DefaultVelocity = camShake.defaultVelocity;
+
+                cmp_CIS.GenerateImpulseWithForce(camShake.impulseForce);
+                break;
+            }
+        }
+    }
 }
