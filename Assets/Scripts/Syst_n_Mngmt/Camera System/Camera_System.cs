29a2217c6a35c965c0b5f527c8b6f537fd36b843
AlekseiPalma <supersonic616@hotmail.com>
2024-06-02 23:07:39 -0500

    Actualizacion de los efectos de camara

--- Assets/Scripts/Syst_n_Mngmt/Camera System/Camera_System.cs
+++ Assets/Scripts/Syst_n_Mngmt/Camera System/Camera_System.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class Camera_System : MonoBehaviour
 {
-    [SerializeField] private CinemachineFreeLook _playerCamera;
+    [SerializeField] private CinemachineFreeLook cmp_playerCamera;
+    [SerializeField] private float defaultFOV;
+    [SerializeField] private List<float> defaultRadius = new List<float>();
     [SerializeField] private Timeline_Manager _timelineManager;
 
     public CameraShakeScriptable[] shakeBehaviors;
@@ -13,7 +15,12 @@ public class Camera_System : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        defaultFOV = cmp_playerCamera.m_Lens.FieldOfView;
+
+        for(int i = 0; i < cmp_playerCamera.m_Orbits.Length; i++)
+        {
+            defaultRadius.Add(cmp_playerCamera.m_Orbits[i].m_Radius);
+        }        
     }
 
     // Update is called once per frame
@@ -22,6 +29,66 @@ public class Camera_System : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.C))
         {
             CameraShake("TestExplosion");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SetFOV(80);
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            StartCoroutine(FOVLerp(20, 2.5f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ResetFOV();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+           ResetFOVLerp(2.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SetRadius(1, 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            StartCoroutine(RadiusLerp(1, 5, 2.5f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ResetRadius(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ResetRadiusLerp(1, 2.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            GlobalRadiusChange(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GlobalRadiusLerp(5, 2.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GlobalRadiusReset();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GlobalRadiusResetLerp(2.5f);
         }
     }
 
@@ -35,8 +102,121 @@ public class Camera_System : MonoBehaviour
                 cmp_CIS.m_DefaultVelocity = camShake.defaultVelocity;
 
                 cmp_CIS.GenerateImpulseWithForce(camShake.impulseForce);
+                return;
+            }
+        }
+        Debug.Log("No existe un efecto de camara llamado " + name);
+    }
+
+    #region FOVChanges
+    public void SetFOV(float FOV)
+    {
+        cmp_playerCamera.m_Lens.FieldOfView = FOV;
+    }
+
+    public IEnumerator FOVLerp(float targetFOV, float lerpDuration)
+    {
+        float t = 0;
+        float currentFOV = cmp_playerCamera.m_Lens.FieldOfView;
+
+        while (true)
+        {
+            yield return null;
+            cmp_playerCamera.m_Lens.FieldOfView = Mathf.Lerp(currentFOV, targetFOV, t / lerpDuration);
+
+            t += Time.deltaTime;
+
+            if (t > lerpDuration)
+            {
                 break;
             }
         }
     }
+
+    public void ResetFOV()
+    {
+        SetFOV(defaultFOV);
+    }
+
+    public void ResetFOVLerp(float lerpDuration)
+    {
+        StartCoroutine(FOVLerp(defaultFOV, lerpDuration));
+    }
+    #endregion
+
+    #region RadiusChange
+    public void SetRadius(int orbitIndex , float radius)
+    {
+        cmp_playerCamera.m_Orbits[orbitIndex].m_Radius = radius;
+    }
+
+    public IEnumerator RadiusLerp(int orbitIndex , float targetRadius, float lerpDuration)
+    {
+        float t = 0;
+        float currentRadius = cmp_playerCamera.m_Orbits[orbitIndex].m_Radius;
+
+        while (true)
+        {
+            yield return null;
+            cmp_playerCamera.m_Orbits[orbitIndex].m_Radius = Mathf.Lerp(currentRadius, targetRadius, t / lerpDuration);
+
+            t += Time.deltaTime;
+
+            if (t > lerpDuration)
+            {
+                break;
+            }
+        }
+    }
+
+    public void ResetRadius(int orbitIndex)
+    {
+        SetRadius(orbitIndex ,defaultRadius[orbitIndex]);
+    }
+
+    public void ResetRadiusLerp(int orbitIndex ,float lerpDuration)
+    {
+        StartCoroutine(RadiusLerp(orbitIndex ,defaultRadius[orbitIndex], lerpDuration));
+    }
+    #endregion
+
+    #region GlobalRadiusChanges
+    public void GlobalRadiusChange(float targetRadius)
+    {
+        float currentRadius = cmp_playerCamera.m_Orbits[1].m_Radius;
+        float difference = targetRadius - currentRadius;
+
+        for (int i = 0; i < cmp_playerCamera.m_Orbits.Length; i++)
+        {
+            SetRadius(i, cmp_playerCamera.m_Orbits[i].m_Radius + difference);
+        }
+    }
+
+    public void GlobalRadiusLerp(float targetRadius, float lerpDuration)
+    {
+        float currentRadius = cmp_playerCamera.m_Orbits[1].m_Radius;
+        float difference = targetRadius - currentRadius;
+
+        for (int i = 0; i < cmp_playerCamera.m_Orbits.Length; i++)
+        {
+            StartCoroutine(RadiusLerp(i, cmp_playerCamera.m_Orbits[i].m_Radius + difference, lerpDuration));
+        }
+    }
+
+    public void GlobalRadiusReset()
+    {
+        for (int i = 0; i < cmp_playerCamera.m_Orbits.Length; i++)
+        {
+            ResetRadius(i);
+        }
+    }
+
+    public void GlobalRadiusResetLerp(float lerpDuration)
+    {
+        for (int i = 0; i < cmp_playerCamera.m_Orbits.Length; i++)
+        {
+            ResetRadiusLerp(i, lerpDuration);
+        }
+    }
+    #endregion
 }
