5dd3eb1181561f75ca8ca1428b7aa70ecaf50315
sebaso <sebastian.olaechea.so@gmail.com>
2024-09-12 21:22:58 +0200

    Agregado el HudHandler

---
+++ Assets/Scripts/Kimera/VisualFeatures/HudHandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Features
+{
+    public class HudHandler :  MonoBehaviour, IActivable, IFeatureSetup,IFeatureUpdate //Other channels
+    {
+        private float hudCurrentFurry;
+        //Configuration
+        [Header("Settings")]
+        public Settings settings;
+        //Control
+        [Header("Control")]
+        [SerializeField] private bool active;
+        //States
+        //Properties
+        //References
+        //Componentes
+
+        public void SetupFeature(Controller controller)
+        {
+            settings = controller.settings;
+
+            //Setup Properties
+
+            ToggleActive(true);
+            hudCurrentFurry=controller.SearchFeature<StatsHandler>().currentFurry;
+        }
+        public void UpdateFeature(Controller controller)
+        {
+            
+        }
+
+        public bool GetActive()
+        {
+            return active;
+        }
+
+        public void ToggleActive(bool active)
+        {
+            this.active = active;
+        }
+
+    }
+}
