89e52887edf76090efe081885c1e0c5072de96b7
Alejandro Medina <almpratto@gmail.com>
2024-11-18 09:04:42 -0500

    fastfix

--- Assets/Scripts/Kimera/Links/AttackLink.cs
+++ Assets/Scripts/Kimera/Links/AttackLink.cs
@@ -86,14 +86,11 @@ namespace Features
                         actorLibrary.CallAudioManager("Impacto");
                         reactorLibrary.CallAudioManager("Daño");
                     }*/
-                    if(attack.Search("attackSFX"))
-                    {
-                        string attackSFX = attack.Search("attackSFX");
 
-                        if (attackSFX != null)
-                            actorLibrary.CallAudioManager(attack.Search("attackSFX"));
-                    }
+                    string attackSFX = attack.Search("attackSFX");
                     
+                    if(attackSFX != null)
+                    actorLibrary.CallAudioManager(attack.Search("attackSFX"));
                     reactorLibrary.CallAudioManager("Daño");
                 }
                 
