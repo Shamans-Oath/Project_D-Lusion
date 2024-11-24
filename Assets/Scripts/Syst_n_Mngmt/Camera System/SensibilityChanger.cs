15483a86a420bf20984f0a20ac8a30f4d9fc34a7
AlekseiPalma <supersonic616@hotmail.com>
2024-11-23 20:33:32 -0500

    Cambios a los limites de sensiblidad y agregado un numero a los slider de las opciones

--- Assets/Scripts/Syst_n_Mngmt/Camera System/SensibilityChanger.cs
+++ Assets/Scripts/Syst_n_Mngmt/Camera System/SensibilityChanger.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class SensibilityChanger : MonoBehaviour
 {
     public Slider slider;
+    public TextMeshProUGUI value;
 
     private void OnEnable()
     {
@@ -22,7 +25,11 @@ public class SensibilityChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float sensRatio = Mathf.InverseLerp(Camera_System.instance.minSens, Camera_System.instance.maxSens, Camera_System.instance.cmp_playerCamera.m_XAxis.m_MaxSpeed);
+
+        int sensValue = (int)Mathf.Lerp(0, 100, sensRatio);
+
+        value.text = sensValue.ToString();
     }
 
     public void UpdateSens(float value)
--- Assets/Scripts/VolumeHandler.cs
+++ Assets/Scripts/VolumeHandler.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class VolumeHandler : MonoBehaviour
 {
     public string VolumeKey;
+    public Slider VolumeSlider;
+    public TextMeshProUGUI value;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,11 @@ public class VolumeHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float sliderRatio = Mathf.InverseLerp(0, 1, VolumeSlider.value);
+        int sliderValue = (int)Mathf.Lerp(0, 100, sliderRatio);
+
+        if(value != null )
+        value.text = sliderValue.ToString();
     }
 
     public void UpdateVolume(float value)
